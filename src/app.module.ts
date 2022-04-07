5b9402589aac88f4a9b2f5bcc8fbf779f4cb54f6
Justin Zhang <jingyu120@gmail.com>
2022-04-07 17:35:25 -0500

    implement create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { User, UserSchema } from './user/schemas/user.schema';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://jingyu120:${process.env.MONGO_PASSWORD}@windbnb.tiffv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    ),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    UserModule,
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
