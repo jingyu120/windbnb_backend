0e50a6511e60e644121da8cb0b66063bfefd7d9a
Justin Zhang <jingyu120@gmail.com>
2022-04-11 22:57:25 -0500

    set up userservice test

--- src/user/users.service.spec.ts
+++ src/user/users.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
 
 describe('UserService', () => {
   let service: UsersService;
+  const mockRepository = {
+    create() {
+      return {};
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(User.name),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
